8191dee587e116b8fb66e61152c4e8b741d82782
adrian3d <adrian3ds@yahoo.fr>
2013-11-14 22:38:07 +0100

    Amélioration du script youtube

--- Labs/xml/youtube.php
+++ Labs/xml/youtube.php
@@ -4,9 +4,9 @@ require_once("Zend/Loader.php");
 Zend_Loader::loadClass("Zend_Gdata_YouTube");
 $yt = new Zend_Gdata_Youtube();
 if (isset($_GET["q"]) && !empty($_GET["q"])) {
-	$videoFeed = $yt->getVideoFeed("http://gdata.youtube.com/feeds/api/videos?q=hero+corp&orderby=published&max-results=10&v=2");
+	$videoFeed = $yt->getVideoFeed("http://gdata.youtube.com/feeds/api/videos?q=".urlencode($_GET["q"])."&orderby=published&max-results=10&v=2&region=FR");
 }else{
-	$videoFeed = $yt->getVideoFeed("http://gdata.youtube.com/feeds/api/standardfeeds/most_popular?min-results=10&v=2");
+	$videoFeed = $yt->getVideoFeed("http://gdata.youtube.com/feeds/api/standardfeeds/FR/most_popular?min-results=10&time=today&v=2");
 }
 $i = 0;
 foreach ($videoFeed as $video): $thumbs = $video->getVideoThumbnails();
