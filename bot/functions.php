9d215d2147dc0c9b332b63d966c1445da0ccf677
adrian3d <adrian3ds@yahoo.fr>
2013-11-16 17:37:18 +0100

    Ajout des classes d'authentification twitter

--- bot/functions.php
+++ bot/functions.php
@@ -5,7 +5,7 @@ $auth =  array(
 	"tw_consumer_secret"    => "vfoA4Ew0f0iv4jioGy96q9RzJknisr3Yy8LMHQEw",
 	"tw_oauth_token"        => "123935211-meMJIWjVL4iaD6mNchXOhbTEYPv5ag4jcfs9wocK",
 	"tw_oauth_token_secret" => "rHm8e8ZRZ6iTl7u0JnPzmTDXDq4N69yTeZoHsQ1wpM",
-	"tw_path_file_oauth"    => "trend/scripts/twitteroauth/twitteroauth.php",
+	"tw_path_file_oauth"    => "auth/twitteroauth/twitteroauth.php",
 	"instg_client_id"       => "9110e8c268384cb79901a96e3a16f588"
 	);
 
