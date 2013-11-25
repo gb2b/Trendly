683b705cab1adf85d2dbf8bcc332ea1b3fa107f7
adrian3d <adrian3ds@yahoo.fr>
2013-11-25 11:11:31 +0100

    Création de mydash.php

---
+++ Trendly/mydash.php
@@ -0,0 +1,222 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+   <meta charset="utf-8">
+   <meta content="IE=edge,chrome=1" http-equiv="X-UA-Compatible">
+
+   <title></title>
+   <meta content="" name="description">
+   <meta content="width=device-width,initial-scale=1" name="viewport">
+   <link href="css/style.css" rel="stylesheet">
+   <link href="css/bootstrap.css" rel="stylesheet">
+</head>
+
+<body>
+   <section class="container">
+   
+   		<div class="content row">
+	   		     	<!-- accordeon with checkbox -->
+   
+    
+    <article class="col-md-4 accordeon check">
+	   			
+	   			 <label for="inner1">PARIS</label>
+	   			 <input id="inner1" type="checkbox" />
+	   			
+	   			<div class="fav-trends inner">
+	   				
+	   				<div class="inner-content">
+		   				<span class="fav-icon"></span>
+		   				<h3>@Le parisien</h3>
+		   				<p>PHOTO A PARTAGER. Tireur fou à #Paris : une nouvelle photo du suspect. Notre DIRECT ici http://t.co/42mEkVpZLI</p>
+		   				
+		   				<div class="action-buttons">
+			   				<span><a href="#" alt="#">Ouvrir</a></span>
+			   				<span><a href="#" alt="#">Partager</a></span>
+			   				<span><a href="#" alt="#">Supprimer</a></span>
+		   				</div>
+	   				</div>
+	   				
+	   				<div class="inner-content">
+		   				<span class="fav-icon"></span>
+		   				<h3>Le monde</h3>
+		   				<p>Philippines : les survivants enterrent leurs morts et 						appellent à l'aide</p>
+		   				
+		   				<div class="action-buttons">
+			   				<span><a href="#" alt="#">Ouvrir</a></span>
+			   				<span><a href="#" alt="#">Partager</a></span>
+			   				<span><a href="#" alt="#">Supprimer</a></span>
+		   				</div>
+	   				</div>
+	   			</div>
+	   			
+	   			
+	   			
+    </article>
+	   		
+	   		<article class="col-md-4 accordeon check">
+	   			
+	   			
+	   			<label for="inner2">#TireurFou</label>
+	   			 <input id="inner2" type="checkbox" />
+	   			
+	   			<div class="fav-trends inner">
+	   				<span class="fav-icon"></span>
+	   				<h3>@Le parisien</h3>
+	   				<p>PHOTO A PARTAGER. Tireur fou à #Paris : une nouvelle photo du suspect. Notre DIRECT ici http://t.co/42mEkVpZLI</p>
+	   				
+	   				<div class="action-buttons">
+		   				<span><a href="#" alt="#">Ouvrir</a></span>
+		   				<span><a href="#" alt="#">Partager</a></span>
+		   				<span><a href="#" alt="#">Supprimer</a></span>
+	   				</div>
+	   			</div>
+	   			
+	   		</article>
+	   		
+	   		<article class="col-md-4 accordeon check">
+	   			
+	   			<label for="inner3">Directionners</label>
+	   			 <input id="inner3" type="checkbox" />
+	   			
+	   			<div class="fav-trends inner">
+	   				<span class="fav-icon"></span>
+	   				<h3>@Le parisien</h3>
+	   				<p>PHOTO A PARTAGER. Tireur fou à #Paris : une nouvelle photo du suspect. Notre DIRECT ici http://t.co/42mEkVpZLI</p>
+	   				
+	   				<div class="action-buttons">
+		   				<span><a href="#" alt="#">Ouvrir</a></span>
+		   				<span><a href="#" alt="#">Partager</a></span>
+		   				<span><a href="#" alt="#">Supprimer</a></span>
+	   				</div>
+	   			</div>
+	   			
+	   		</article>
+	   		
+	   		
+	   		<article class="col-md-4 accordeon check">
+	   			
+	   			 <label for="inner4">PARIS</label>
+	   			 <input id="inner4" type="checkbox" />
+	   			
+	   			<div class="fav-trends inner">
+	   				<span class="fav-icon"></span>
+	   				<h3>@Le parisien</h3>
+	   				<p>PHOTO A PARTAGER. Tireur fou à #Paris : une nouvelle photo du suspect. Notre DIRECT ici http://t.co/42mEkVpZLI</p>
+	   				
+	   				<div class="action-buttons">
+		   				<span><a href="#" alt="#">Ouvrir</a></span>
+		   				<span><a href="#" alt="#">Partager</a></span>
+		   				<span><a href="#" alt="#">Supprimer</a></span>
+	   				</div>
+	   			</div>
+    </article>
+	   		
+	   		<article class="col-md-4 accordeon check">
+	   			
+	   			
+	   			<label for="inner5">#TireurFou</label>
+	   			 <input id="inner5" type="checkbox" />
+	   			
+	   			<div class="fav-trends inner">
+	   				<span class="fav-icon"></span>
+	   				<h3>@Le parisien</h3>
+	   				<p>PHOTO A PARTAGER. Tireur fou à #Paris : une nouvelle photo du suspect. Notre DIRECT ici http://t.co/42mEkVpZLI</p>
+	   				
+	   				<div class="action-buttons">
+		   				<span><a href="#" alt="#">Ouvrir</a></span>
+		   				<span><a href="#" alt="#">Partager</a></span>
+		   				<span><a href="#" alt="#">Supprimer</a></span>
+	   				</div>
+	   			</div>
+	   			
+	   		</article>
+	   		
+	   		<article class="col-md-4 accordeon check">
+	   			
+	   			<label for="inner6">Directionners</label>
+	   			 <input id="inner6" type="checkbox" />
+	   			
+	   			<div class="fav-trends inner">
+	   				<span class="fav-icon"></span>
+	   				<h3>@Le parisien</h3>
+	   				<p>PHOTO A PARTAGER. Tireur fou à #Paris : une nouvelle photo du suspect. Notre DIRECT ici http://t.co/42mEkVpZLI</p>
+	   				
+	   				<div class="action-buttons">
+		   				<span><a href="#" alt="#">Ouvrir</a></span>
+		   				<span><a href="#" alt="#">Partager</a></span>
+		   				<span><a href="#" alt="#">Supprimer</a></span>
+	   				</div>
+	   			</div>
+	   			
+	   		</article>
+	   		
+	   		<article class="col-md-4 accordeon check">
+	   			
+	   			 <label for="inner7">PARIS</label>
+	   			 <input id="inner7" type="checkbox" />
+	   			
+	   			<div class="fav-trends inner">
+	   				<span class="fav-icon"></span>
+	   				<h3>@Le parisien</h3>
+	   				<p>PHOTO A PARTAGER. Tireur fou à #Paris : une nouvelle photo du suspect. Notre DIRECT ici http://t.co/42mEkVpZLI</p>
+	   				
+	   				<div class="action-buttons">
+		   				<span><a href="#" alt="#">Ouvrir</a></span>
+		   				<span><a href="#" alt="#">Partager</a></span>
+		   				<span><a href="#" alt="#">Supprimer</a></span>
+	   				</div>
+	   			</div>
+    </article>
+	   		
+	   		<article class="col-md-4 accordeon check">
+	   			
+	   			
+	   			<label for="inner8">#TireurFou</label>
+	   			 <input id="inner8" type="checkbox" />
+	   			
+	   			<div class="fav-trends inner">
+	   				<span class="fav-icon"></span>
+	   				<h3>@Le parisien</h3>
+	   				<p>PHOTO A PARTAGER. Tireur fou à #Paris : une nouvelle photo du suspect. Notre DIRECT ici http://t.co/42mEkVpZLI</p>
+	   				
+	   				<div class="action-buttons">
+		   				<span><a href="#" alt="#">Ouvrir</a></span>
+		   				<span><a href="#" alt="#">Partager</a></span>
+		   				<span><a href="#" alt="#">Supprimer</a></span>
+	   				</div>
+	   			</div>
+	   			
+	   			
+	   			
+	   		</article>
+	   		
+	   		<article class="col-md-4 accordeon check">
+	   			
+	   			<label for="inner9">Directionners</label>
+	   			 <input id="inner9" type="checkbox" />
+	   			
+	   			<div class="fav-trends inner">
+	   				<span class="fav-icon"></span>
+	   				<h3>@Le parisien</h3>
+	   				<p>PHOTO A PARTAGER. Tireur fou à #Paris : une nouvelle photo du suspect. Notre DIRECT ici http://t.co/42mEkVpZLI</p>
+	   				
+	   				<div class="action-buttons">
+		   				<span><a href="#" alt="#">Ouvrir</a></span>
+		   				<span><a href="#" alt="#">Partager</a></span>
+		   				<span><a href="#" alt="#">Supprimer</a></span>
+	   				</div>
+	   			</div>
+	   			
+	   		</article>
+     	 
+     	       
+   </section>
+</body>
+<script src="js/jquery.js"></script>
+ 
+<script src="js/local_storage.js"></script>
+<script src="js/my_local_storage.js"></script>
+
+</html>
