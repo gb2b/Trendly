81a4881cae3b0c8a048af34ab2d10f4d143bec1b
adrian3d <adrian3ds@yahoo.fr>
2013-11-29 00:11:02 +0100

    Propreté du code amélioré

--- Trendly/dashboard.php
+++ Trendly/dashboard.php
@@ -104,18 +104,17 @@
 
    	$("body").on('click', '.delete-btn', function(event) {
    		  	var trendname = document.querySelector('.trendname').innerHTML;
-
-		$("#listnotif").append("<li class=\"notif\"><p>Votre Trendlist \""+ trendname +"\" a bien été supprimée !  <i class=\"glyphicon glyphicon-remove-circle close\"></i></p></li>");
-		$(".notif").fadeIn(500);
-		$(".notif").on('click','.close',function(event){
+			$("#listnotif").append("<li class=\"notif\"><p>Votre Trendlist \""+ trendname +"\" a bien été supprimée !  <i class=\"glyphicon glyphicon-remove-circle close\"></i></p></li>");
+			$(".notif").fadeIn(500);
+			$(".notif").on('click','.close',function(event){
 	  		$(this).parent().parent().fadeOut(500);
 		})
   	});
   	$("body").on('click', '.delete', function(event) {
   			var trendname = document.querySelector('.trendname').innerHTML;
-		$("#listnotif").append("<li class=\"notif\"><p>Votre contenu pour \""+ trendname +"\" a bien été supprimé !  <i class=\"glyphicon glyphicon-remove-circle close\"></i></p></li>");
-		$(".notif").fadeIn(500);
-		$(".notif").on('click','.close',function(event){
+			$("#listnotif").append("<li class=\"notif\"><p>Votre contenu pour \""+ trendname +"\" a bien été supprimé !  <i class=\"glyphicon glyphicon-remove-circle close\"></i></p></li>");
+			$(".notif").fadeIn(500);
+			$(".notif").on('click','.close',function(event){
 	  		$(this).parent().parent().fadeOut(500);
 	  		$(this).parent().parent().remove();
 		})
