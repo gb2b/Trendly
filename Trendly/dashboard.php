2e08ea60139f8786bc97da90540cec4283b37ec6
adrian3d <adrian3ds@yahoo.fr>
2013-11-29 00:12:12 +0100

    Propretée du code améliorée une fois de plus

--- Trendly/dashboard.php
+++ Trendly/dashboard.php
@@ -99,27 +99,7 @@
 	</section>
 </body>
 <script src="js/jquery.js"></script>
-<script>
-	  //Notification pour un contenu supprimé
-
-   	$("body").on('click', '.delete-btn', function(event) {
-   		  	var trendname = document.querySelector('.trendname').innerHTML;
-			$("#listnotif").append("<li class=\"notif\"><p>Votre Trendlist \""+ trendname +"\" a bien été supprimée !  <i class=\"glyphicon glyphicon-remove-circle close\"></i></p></li>");
-			$(".notif").fadeIn(500);
-			$(".notif").on('click','.close',function(event){
-	  		$(this).parent().parent().fadeOut(500);
-		})
-  	});
-  	$("body").on('click', '.delete', function(event) {
-  			var trendname = document.querySelector('.trendname').innerHTML;
-			$("#listnotif").append("<li class=\"notif\"><p>Votre contenu pour \""+ trendname +"\" a bien été supprimé !  <i class=\"glyphicon glyphicon-remove-circle close\"></i></p></li>");
-			$(".notif").fadeIn(500);
-			$(".notif").on('click','.close',function(event){
-	  		$(this).parent().parent().fadeOut(500);
-	  		$(this).parent().parent().remove();
-		})
-  	});
-</script>
+<script src="js/notifications"></script>
 <script src="js/mustache.js"></script>
 
 <script src="js/local_storage.js"></script>
