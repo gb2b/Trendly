021afa529966857891a27f0a1e1fea23cea1f51c
adrian3d <adrian3ds@yahoo.fr>
2013-11-29 01:57:53 +0100

    Commentaires sur les codes js de adrian

--- Trendly/js/dashboard.js
+++ Trendly/js/dashboard.js
@@ -1,18 +1,25 @@
 jQuery(document).ready(function($) {
+	//Initiates an object that handles the localstorage
 	storage.init();
+	//Receives the data as an Array in the localstorage.
 	var datas = storage.render();
+	//Fetches the templates from the page's php.
 	var templateUpTrend = $('#trendUpTlp').html();
-	var templateTrend = $('#trendTlp').html();
-	var colors = ["flat-red", "flat-green", "flat-blue", "flat-yellow"];
-	var colori = 0;
+	var templateTrend   = $('#trendTlp').html();
+	var colors          = ["flat-red", "flat-green", "flat-blue", "flat-yellow"];
+	var colori          = 0;
+	//Runs through the successive lignes of the localstorage.
 	for (var i = 0; i < datas.length; i++) {
 		var inner = "inner"+i;
 		var data = JSON.parse(datas[i]);
+		//Adds data in the templates via Mustache, then inserts the templates in the page.
 		var html = Mustache.to_html(templateUpTrend, data[0]);
 		$('.content').append(html);
+		//Réglages de détails d'affichage Settings for the 'detail' display
 		$('article').eq(i).find('input').attr('id', inner);
 		$('article').eq(i).find('label').attr('for', inner);
 		$('.dropdown-btn').eq(i).addClass(colors[colori]);
+		//Runs through the successive values of the line from localstorage.
 		for (var j = 0; j < data.length; j++) {
 			var html2 = Mustache.to_html(templateTrend, data[j]);
 			$('.inner').eq(i).append(html2);
@@ -23,12 +30,13 @@ jQuery(document).ready(function($) {
 			colori++;
 		}
 	}
-
+	//On click of link classed as .delete-btn, the ligne is deleted from the localstorage, and we get visual feedback.
 	$('body').on('click', '.delete-btn', function(event) {
 		event.preventDefault();
 		storage.deleteKey($(this).data('trend'));
 		$(this).parent("article").remove();
 	});
+	//On click of link marked as .span-delete, the value of the corresponding ligne is deleted from the localstorage, and we get visual feedback.
 	$('body').on('click', 'span.delete', function(event) {
 		event.preventDefault();
 		if (storage.deleteValueOfKey($(this).data('trend'), parseInt($(this).data('key')))) {
--- Trendly/js/local_storage.js
+++ Trendly/js/local_storage.js
@@ -6,22 +6,22 @@ var storage = {
 		this.params=$.extend(this.defaults,options);
 		return this;
 	},
-	//record function record datas in local storage
+	//'record' function saves the data in the local storage
 	record : function(datas){
-		//récupération des données correspondante à datas.title et ajout newLocalStorage
+		//Fetches all data from datas.title and adds newLocalStorage
 		var actualLocalStorage = $.parseJSON(this.getItem(datas.title));
 		if (datas.title.length > 0) {
 			var newLocalStorage = $.makeArray(actualLocalStorage);
 		}else{
 			var newLocalStorage = new Array;
 		}
-		//ajout des datas passées en paramètre dans le tableau newLocalStorage
+		//Adds all data passed as parameter to the newLocalStorage table.
 		newLocalStorage.push(datas);
 		//newLocalStorage est reparsé en JSON puis remplace le localstorage d'index datas.title
 		localStorage.setItem(datas.title,JSON.stringify(newLocalStorage));
 		this.params.recorded.call(this,datas);
 	},
-	//render envoie un tableau contenant les données du localstorage d'index datas.title
+	//render pushes an array containing all the the data from the localstorage indexed as 'datas.title'
 	render : function (title){
 		var datas = new Array();
 		if (title != null) {
@@ -33,9 +33,9 @@ var storage = {
 		}
 		return datas;
 	},
-	//Supprime une valeur d'une ligne du localstorage d'index datas.title
+	//Deletes the value of a line from the localstorage indexed as datas.title
 	deleteValueOfKey : function(title, key){
-		//Analyse des données de la ligne du localstorage renvoyée et suppression de la valeur correspondante à datas.key
+		//Reads the data from the localstorage sent back and deletes the value corresponding to datas.key.
 		var actualLocalStorage = $.parseJSON(this.render(title));
 		for (var i = 0; i < actualLocalStorage.length; i++) {
 			if (actualLocalStorage[i].key == key) {
@@ -43,7 +43,7 @@ var storage = {
 			}
 		}
 		actualLocalStorage = $.makeArray(actualLocalStorage);
-		//Si il n'y a plus de valeur dans la ligne du localstorage, alors la ligne entière est supprimée.
+		//If there are no mo values in the localstorage line, the line as a whole is deleted.
 		if (actualLocalStorage.length!=0) {
 			localStorage.setItem(title,JSON.stringify(actualLocalStorage));
 			return false;
@@ -52,11 +52,11 @@ var storage = {
 			return true;
 		}
 	},
-	//Suppression d'une ligne du localstorage
+	//Deletes a line from the localstorage
 	deleteKey : function(title){
 		localStorage.removeItem(title);
 	},
-	//Récupération d'une ligne du localstorage en fonction du nom de la ligne
+	//Fetches a line from the localstorage depending on the name of the line
 	getItem : function (title) {
 		return localStorage.getItem(title);
 	}
